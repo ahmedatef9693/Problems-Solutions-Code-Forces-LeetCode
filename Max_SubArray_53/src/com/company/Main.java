345e2dc75929f218b3c99c385cadd44eec970966
ahmed atef <99121164+ahmedatef9693@users.noreply.github.com>
2022-07-16 04:59:36 +0200

    Update Main.java

--- Max_SubArray_53/src/com/company/Main.java
+++ Max_SubArray_53/src/com/company/Main.java
@@ -2,36 +2,6 @@ package com.company;
 
 public class Main {
 
-    public static int Max_Subarray_Sum(int arr[] ,int start, int end){
-
-        if(arr.length == 1){
-            return arr[0];
-        }
-        int mid = arr.length/2;
-        int starting_Index = 0;
-        int endingIndex = arr.length-1;
-        int left_sum = Integer.MIN_VALUE;
-        int right_sum = Integer.MIN_VALUE;
-        int sum = 0;
-        int left_MSS = Max_Subarray_Sum(arr , starting_Index , mid);
-        int right_MSS = Max_Subarray_Sum(arr ,mid+1 , endingIndex);
-
-        //getting left sum
-        for (int i = 0; i <mid-1 ; i++) {
-            sum += arr[i];
-            left_sum = Math.max(sum , left_sum);
-        }
-        sum = 0;
-        //getting right sum
-        for (int j = mid; j < arr.length ; j++) {
-            sum += arr[j];
-            right_sum = Math.max(sum , right_sum);
-        }
-
-        int answer = Math.max(left_MSS,right_MSS);
-        return Math.max(answer,left_sum+right_sum);
-    }
-
     public static int maxSubArray(int[] nums) {
         int sum = 0;
         int val = Integer.MIN_VALUE;
